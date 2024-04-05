e3ace621417905bfc37755d2a16e1fc47e534ee5
Toucan4Life <tdp94@hotmail.com>
2024-04-05 14:29:52 +0000

    compute jaccard similarity

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
+use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::fs::File;
+use std::hash::RandomState;
 use std::io::{prelude::*, BufReader};
+
 #[derive(Serialize, Deserialize)]
 struct FullRating {
     bgg_id: i32,
@@ -20,15 +24,65 @@ struct Rating {
     bgg_user_name: String,
 }
 fn main() {
+    //get output
     let output = "bgg_RatingItemDecimated.jl";
+    println!("Decimating input");
     //decimate("bgg_RatingItem.jl", output);
+    println!("Reading new input");
     let file = File::open(output).unwrap();
     let ratings: Vec<Rating> = serde_json::from_reader(file).unwrap();
-    println!("{}", ratings.len());
-    let binding = ratings.iter().into_group_map_by(|rating| rating.bgg_user_name.clone());
-    for gr in binding.values().filter(|vec| vec.len()>1){
-        println!("{:?}", gr)
-    }
+    println!("Number of ratings : {}", ratings.len());
+    //Group rating by game
+    let binding = ratings
+        .iter()
+        .into_group_map_by(|rating| rating.bgg_id.clone());
+
+    let binding: HashMap<&i32, HashSet<String>, RandomState> =
+        HashMap::from_iter(binding.iter().filter(|test| test.1.len() > 20).map(
+            |(boardgame, vec)| {
+                (
+                    boardgame,
+                    HashSet::from_iter(vec.iter().map(|t| t.bgg_user_name.clone())),
+                )
+            },
+        ));
+    println!(
+        "Number of games with more than 20 ratings : {}",
+        binding.len()
+    );
+    //compute jaccard similarity
+    let binding2 = binding.keys().tuple_combinations().collect_vec();
+    println!("Number of combinations : {}", binding2.len());
+    println!("Starting Jaccard");
+    let jaccard_sets = binding2
+        .par_iter()
+        .map(|(&set1, &set2)| (set1, set2, jaccard(&binding[set1], &binding[set2])))
+        .filter(|(&set1, &set2, jac)| jac.ne(&1.0));
+    let binding = jaccard_sets.collect::<Vec<_>>();
+    let sorted_by = binding
+        .iter()
+        .sorted_by(|a, b| b.2.partial_cmp(&a.2).unwrap())
+        .take(100000)
+        .collect_vec();
+
+    sorted_by
+        .iter()
+        .take(10)
+        .for_each(|chosen_one| println!("{}, {}, {}", chosen_one.0, chosen_one.1, chosen_one.2));
+    
+    println!("Writing File");
+    let file = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("bgg_RatingJaccard.jl")
+        .unwrap();
+
+    serde_json::to_writer(file, &sorted_by).unwrap();
+}
+
+fn jaccard(set1: &HashSet<String>, set2: &HashSet<String>) -> f32 {
+    (set1.intersection(&set2).count() as f32) / (set1.union(&set2).count() as f32)
 }
 
 fn decimate(input: &str, output: &str) {
@@ -37,17 +91,19 @@ fn decimate(input: &str, output: &str) {
     let decimated: Vec<_> = reader
         .lines()
         .enumerate()
+        //.par_bridge().into_par_iter()
         .filter_map(|(i, rating)| {
             let var_name = rating.unwrap();
-            if i % 1000 != 0 {
+            if i % 10 != 0 {
                 return None;
             }
             match serde_json::from_str::<FullRating>(&var_name) {
-
                 Ok(t) => {
-                    if t.bgg_user_rating == None{return None}
+                    if t.bgg_user_rating == None {
+                        return None;
+                    }
                     //println!("{}", var_name);
-                    return Some(t)
+                    return Some(t);
                 }
                 Err(_) => {
                     println!("{}", var_name);
